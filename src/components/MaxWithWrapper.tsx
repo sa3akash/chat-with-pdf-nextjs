848d21246a34a8f5a47353b56a0f9b66836cc108
sa3akash <sa3shakil@gmail.com>
2023-10-15 21:25:30 +0600

    Project setup

---
+++ src/components/MaxWithWrapper.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface MaxWidthWrapper {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const MaxWithWrapper: React.FC<MaxWidthWrapper> = ({ children, className }) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWithWrapper;
